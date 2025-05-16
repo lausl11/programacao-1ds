bd635a2f688ad1809b52c7dccbd734fa595f0ca6
lausl11 <laura.laurindo@escola.pr.gov.br>
2025-05-16 13:27:36 -0300

    Create teste-input.py

---
+++ teste-input.py
@@ -0,0 +1,8 @@
+# Solicita nome e idade do usuário
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+# Converte a idade para um inteiro
+idade = int(idade)
+
+print(f"Olá {nome}, você tem {idade} anos.")
