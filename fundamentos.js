a47de1653025bc908e4d673da5785d39e7bca4a3
lausl11 <laura.laurindo@escola.pr.gov.br>
2025-09-21 15:03:36 -0300

    Create fundamentos.js

---
+++ fundamentos.js
@@ -0,0 +1,51 @@
+let idade = 30;
+console.log(idade)
+
+
+function soma(num1, num2){
+return num1+num2;
+}
+let num1 = 2
+let num2 = 3 
+console.log(soma(num1, num2));
+
+
+let num = 11;
+if (num % 2 ===0){
+   console.log("o número é par")
+}else{
+    console.log("o número é impar")
+}
+
+
+for(let i = 65; i<=90;i++){
+    console.log(String.fromCharCode(i));
+}
+
+
+let n1=20;
+let n2 =10;
+let n3 = 10;
+
+let media =(n1+n2+n3) / 3;
+ console.log (media)
+
+
+console.log("ola mundo!")
+
+
+let i =5;
+while (i>=0){
+    console.log(i)
+    i--;
+}
+
+
+function calcularProduto(a, b) {
+  let resultado = a * b;
+  debugger;
+  return resultado;
+}
+// Para ver o debugger em ação, chame a função e abra as Ferramentas do Desenvolvedor no navegador.
+calcularProduto(5, 10);
+
